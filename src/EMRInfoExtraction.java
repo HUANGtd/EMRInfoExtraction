e785d8778915c24e3918a4342b9bce6b12970c03
HUANG.tudou <HUANGtd@users.noreply.github.com>
2016-05-09 20:49:00 +0800

    Revert "Revert "Dev""

--- src/EMRInfoExtraction.java
+++ src/EMRInfoExtraction.java
@@ -1,27 +1,13 @@
-import basic.EMRInfoTree;
-import io.DataInput;
-import io.DataOutput;
+import basic.EMRInfoExtractionTask;
 
 /**
- * Created by hhw on 4/21/16.
+ * Created by huang.tudou
  */
 public class EMRInfoExtraction {
     public static void main(String args[]) {
-        EMRInfoTree tree = new EMRInfoTree();
-        DataInput dict = new DataInput("data/category.txt");
-
-        for(int i = 1; i < 10; i++) {
-            // import dictionary and generate tree
-            tree = new EMRInfoTree();
-            dict.GenerateEMRInfoTree(tree);
-
-            // infomation extraction
-            DataInput data = new DataInput("data/data/data" + i + ".xml");
-            tree.parseEMRData(data.EMRDataReader());
-
-            // out put
-            DataOutput dop = new DataOutput("data/result/result" + i + ".txt");
-            dop.OutPut2txt(tree.toString());
-        }
+        EMRInfoExtractionTask task1 = new EMRInfoExtractionTask("data/dictionary/txt/admission.txt", "data/emr/admission.xml", "data/result/r_admission.txt");
+        task1.genEMRTree();
+        task1.extractionInfo();
+        task1.output2txt();
     }
 }
